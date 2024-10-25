f9e28189f3a132edf60eba16db91038d22e225c4
arshad012 <forarshad99@gmail.com>
2024-10-26 00:00:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+
+document.getElementById('delete').addEventListener('click', () => {
+	const color = document.getElementById('colorSelect').value;
+
+	document.getElementById(color).remove();
+})
+
